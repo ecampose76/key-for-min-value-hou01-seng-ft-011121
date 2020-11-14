024a0169b2106864ae5febe8a417e6869f50e58a
Elias Campos <ecampose76@gmail.com>
2020-11-14 01:53:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,10 @@
 
 def key_for_min_value(name_hash)
 
-name_hash.each do |min_value|
-  if min_value.is_a? Min 
-    p min_value
-
-
+name_answer = ""
+name_hash.each do |name, num|
+  if num 
+    name_answer = name
+  end
+end
 end

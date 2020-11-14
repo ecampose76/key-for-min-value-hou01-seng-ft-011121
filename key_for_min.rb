7efa5767b1583de0275d77b5a6f460a672f645c4
Elias Campos <ecampose76@gmail.com>
2020-11-14 02:19:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,16 @@
 def key_for_min_value(name_hash)
 
 name_hash.each do |key, value|
-  
+  def key_for_min_value(name_hash)
+  num = 999999 #name_hash.select {|key, value| value}
+  name_hash.each do |key, value|
+    if name_hash[key] < num
+      num = name_hash[key]
+      answer = key
+    end
+  end
+ answer
+end
 
 end
 
